0ba053cb87ba00adce75f0b83dc078c77a945793
Sean-mn <b01080080329@gmail.com>
2025-07-13 22:58:15 +0900

    feat: AccountService 구현
    #1

---
+++ PaperMania/Server/Infrastructure/Service/AccountService.cs
@@ -0,0 +1,58 @@
+﻿using Server.Application.Port;
+using Server.Domain.Entity;
+using Server.Infrastructure.Service.Interface;
+
+namespace Server.Infrastructure.Service;
+
+public class AccountService : IAccountService
+{
+    private readonly IAccountRepository _repository;
+    private readonly ISessionService _sessionService;
+
+    public AccountService(IAccountRepository repository, ISessionService sessionService)
+    {
+        _repository = repository;
+        _sessionService = sessionService;
+    }
+    
+    public async Task<PlayerAccountData?> GetByPlayerIdAsync(string playerId)
+    {
+        return await _repository.GetByPlayerIdAsync(playerId);
+    }
+
+    public async Task<PlayerAccountData?> GetByEmailAsync(string email)
+    {
+        return await _repository.GetByEmailAsync(email);
+    }
+
+    public async Task RegisterAsync(PlayerAccountData player, string password)
+    {
+        player.Password = BCrypt.Net.BCrypt.HashPassword(password);
+        player.IsNewAccount = true;
+        player.Role = "user";
+        
+        await _repository.AddAccountAsync(player);
+    }
+
+    public async Task<string> LoginAsync(string playerId, string password)
+    {
+        var user = await _repository.GetByPlayerIdAsync(playerId);
+        if (user == null) 
+            return string.Empty;
+        
+        bool isVerified = BCrypt.Net.BCrypt.Verify(password, user.Password);
+        if (!isVerified)
+            return string.Empty;
+        
+        await UpdateLastLoginAsync(user.Id);
+
+        var sessionId = await _sessionService.CreateSessionAsync(user.Id);
+        
+        return sessionId;
+    }
+
+    public async Task UpdateLastLoginAsync(int playerId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PaperMania/Server/Infrastructure/Service/Interface/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using Server.Domain.Entity;
+
+namespace Server.Infrastructure.Service.Interface;
+
+public interface IAccountService
+{
+    Task<PlayerAccountData?> GetByPlayerIdAsync(string playerId);
+    Task<PlayerAccountData?> GetByEmailAsync(string email);
+    Task RegisterAsync(PlayerAccountData player, string password);
+    Task<string> LoginAsync(string playerId, string password);
+    Task UpdateLastLoginAsync(int playerId);
+}
