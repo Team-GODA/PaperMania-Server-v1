2e0e982d3396203b5ace1a23f9b2cfde0062e358
Sean-mn <b01080080329@gmail.com>
2025-07-13 21:58:21 +0900

    feat: PlayerAccountData 엔티티 추가
    #1

---
+++ PaperMania/Server/Domain/Entity/PlayerAccountData.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Domain.Entity;
+
+public class PlayerAccountData
+{
+    public int Id { get; set; }
+    public string PlayerId { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public bool IsNewAccount { get; set; } = true;
+    public string Role { get; set; } = null!;
+}
