c17b56959e6c32c19cf6201c24077f7e9c953abe
Sean-mn <b01080080329@gmail.com>
2025-07-13 22:31:26 +0900

    feat: CacheService 구현
    #1

---
+++ PaperMania/Server/Infrastructure/Service/CacheService.cs
@@ -0,0 +1,35 @@
+﻿using Server.Infrastructure.Service.Interface;
+using StackExchange.Redis;
+
+namespace Server.Infrastructure.Service;
+
+public class CacheService : ICacheService
+{
+    private IDatabase _db;
+
+    public CacheService(IConnectionMultiplexer redis)
+    {
+        _db = redis.GetDatabase();
+    }
+    
+    public async Task SetAsync(string key, string value, TimeSpan? expiration = null)
+    {
+        _db.StringSetAsync(key, value, expiration);
+    }
+
+    public async Task<string?> GetAsync(string key)
+    {
+        var value = await _db.StringGetAsync(key);
+        return value.IsNullOrEmpty ? null : value.ToString();
+    }
+
+    public async Task RemoveAsync(string key)
+    {
+        _db.KeyDeleteAsync(key);
+    }
+
+    public async Task<bool> ExistsAsync(string key)
+    {
+        return await _db.KeyExistsAsync(key);
+    }
+}
---
+++ PaperMania/Server/Infrastructure/Service/Interface/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Infrastructure.Service.Interface;
+
+public interface ICacheService
+{
+    Task SetAsync(string key, string value, TimeSpan? expiration = null);
+    Task<string?> GetAsync(string key);
+    Task RemoveAsync(string key);
+    Task<bool> ExistsAsync(string key);
+}
