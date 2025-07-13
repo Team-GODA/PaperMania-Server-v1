f45778e38930ee0127dbc06e6f8f8ba4824755f0
Sean-mn <b01080080329@gmail.com>
2025-07-14 02:15:24 +0900

    feat: 플레이어 데이터 엔티티 생성
    #4

---
+++ PaperMania/Server/Domain/Entity/PlayerGameData.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Domain.Entity;
+
+public class PlayerGameData
+{
+    public int Id { get; set; }
+    public string PlayerName { get; set; } = null!;
+    public int PlayerExp { get; set; } = 0;
+    public int PlayerLevel { get; set; } = 1;
+}
