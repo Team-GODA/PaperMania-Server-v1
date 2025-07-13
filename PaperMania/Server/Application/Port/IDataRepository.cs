d0bc5e22c2d77eab98cc28086f2f3722da469311
Sean-mn <b01080080329@gmail.com>
2025-07-14 02:15:45 +0900

    feat: DataRepository 구현
    #4

---
+++ PaperMania/Server/Application/Port/IDataRepository.cs
@@ -0,0 +1,11 @@
+﻿using Server.Domain.Entity;
+
+namespace Server.Application.Port;
+
+public interface IDataRepository
+{
+    Task<PlayerGameData?> ExistsPlayerNameAsync(string playerName);
+    Task<bool> IsNewAccountAsync(string playerId);
+    Task AddPlayerNameAsync(string playerName);
+    Task UpdateIsNewAccountAsync(string playerId, bool isNew = true);
+}
