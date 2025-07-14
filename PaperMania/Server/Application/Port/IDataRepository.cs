b46e7d8c59cd8fbcfa800dd9d90a6889b43e757f
Sean-mn <b01080080329@gmail.com>
2025-07-14 22:43:20 +0900

    refactor: accountRepo, dataRepo 리팩토링
    - IsNewAccount 관련 매서드 AccountRepository로 이전
    
    #7

--- PaperMania/Server/Application/Port/IDataRepository.cs
+++ PaperMania/Server/Application/Port/IDataRepository.cs
@@ -5,7 +5,5 @@ namespace Server.Application.Port;
 public interface IDataRepository
 {
     Task<PlayerGameData?> ExistsPlayerNameAsync(string playerName);
-    Task<bool> IsNewAccountAsync(int? userId);
     Task AddPlayerNameAsync(string playerName);
-    Task UpdateIsNewAccountAsync(int? userId, bool isNew = true);
 }
