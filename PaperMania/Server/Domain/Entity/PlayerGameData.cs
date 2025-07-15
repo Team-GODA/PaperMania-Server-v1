f5c1c2c27f9438d2d93a63256579a69db25b69b3
Sean-mn <b01080080329@gmail.com>
2025-07-16 02:06:15 +0900

    feat: PlayerGameData 속성 추가
    #10

--- PaperMania/Server/Domain/Entity/PlayerGameData.cs
+++ PaperMania/Server/Domain/Entity/PlayerGameData.cs
@@ -3,6 +3,8 @@
 public class PlayerGameData
 {
     public int Id { get; set; }
+    public string PlayerId { get; set; } = null!;
+    public string Role { get; set; } = null!;
     public string PlayerName { get; set; } = null!;
     public int PlayerExp { get; set; } = 0;
     public int PlayerLevel { get; set; } = 1;
