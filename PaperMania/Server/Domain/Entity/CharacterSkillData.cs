f8ac048cccaeb15c488ee3a5f970a0d669d90a0b
Sean-mn <b01080080329@gmail.com>
2025-07-17 20:24:28 +0900

    delete: 필요없는 클래스 삭제
    #25

--- PaperMania/Server/Domain/Entity/CharacterSkillData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Server.Domain.Entity;
-
-public class CharacterSkillData
-{
-    public int Id { get; set; }
-    public string CharacterId { get; set; } = null!;
-    public int NormalSkillLevel { get; set; } = 0;
-    public int EpicSkillLevel { get; set; } = 0;
-}
