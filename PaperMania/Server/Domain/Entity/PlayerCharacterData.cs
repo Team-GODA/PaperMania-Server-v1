9a07f973434202aeb875585df2a6f2ba32f5cce1
Sean-mn <b01080080329@gmail.com>
2025-07-17 16:28:33 +0900

    feat: PlayerCharacterData 속성 추가
    #23

--- PaperMania/Server/Domain/Entity/PlayerCharacterData.cs
+++ PaperMania/Server/Domain/Entity/PlayerCharacterData.cs
@@ -6,4 +6,12 @@ public class PlayerCharacterData
     public string CharacterId { get; set; } = null!;
     public string CharacterName { get; set; } = null!;
     public int CharacterLevel { get; set; } = 1;
+    public string RarityString
+    {
+        get => Rarity.ToString();
+        set => Rarity = Enum.Parse<Rarity>(value);
+    }
+    
+    [System.Text.Json.Serialization.JsonIgnore]
+    public Rarity Rarity { get; set; }
 }
