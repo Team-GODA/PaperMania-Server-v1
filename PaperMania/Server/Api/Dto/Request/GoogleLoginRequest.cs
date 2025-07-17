6a30274486655ce33d049757c7f5b27d8984d8ed
Sean-mn <b01080080329@gmail.com>
2025-07-18 08:27:24 +0900

    feat: 구글 로그인 요청/응답 Dto 생성
    #31

---
+++ PaperMania/Server/Api/Dto/Request/GoogleLoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Api.Dto.Request;
+
+public class GoogleLoginRequest
+{
+    public string IdToken { get; set; } = null!;
+    public string PlayerId { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
