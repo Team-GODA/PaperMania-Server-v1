7edea5aa320150894b289d2143a7a5728d28dea6
Sean-mn <b01080080329@gmail.com>
2025-07-17 14:04:40 +0900

    feat: 열거형 문자열 받기 수정
    - 엔티티 열거형 문자열로 자료 받기
    
    #23

--- PaperMania/Server/Domain/Entity/CharacterData.cs
+++ PaperMania/Server/Domain/Entity/CharacterData.cs
@@ -4,6 +4,14 @@ public class CharacterData
 {
     public string CharacterId { get; set; } = null!;
     public string CharacterName { get; set; } = null!;
+
+    public string RarityString
+    {
+        get => Rarity.ToString();
+        set => Rarity = Enum.Parse<Rarity>(value);
+    }
+    
+    [System.Text.Json.Serialization.JsonIgnore]
     public Rarity Rarity { get; set; }
 }
 
--- PaperMania/Server/Domain/Entity/SkillData.cs
+++ PaperMania/Server/Domain/Entity/SkillData.cs
@@ -4,6 +4,14 @@ public class SkillData
 {
     public string SkillId { get; set; } = null!;
     public string SkillName { get; set; } = null!;
+    
+    public string SkillTypeString
+    {
+        get => SkillType.ToString();
+        set => SkillType = Enum.Parse<SkillType>(value);
+    }
+    
+    [System.Text.Json.Serialization.JsonIgnore]
     public SkillType SkillType { get; set; }
 }
 
