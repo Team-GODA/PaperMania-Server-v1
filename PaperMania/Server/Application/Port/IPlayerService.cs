40bae9bde53b32a16d69594e97206e6a1adbd40f
Seanyee1227 <b01080080329@gmail.com>
2025-07-19 00:51:37 +0900

    feat: 플레이어 생성 서비스 구현
    #39

---
+++ PaperMania/Server/Application/Port/IPlayerService.cs
@@ -0,0 +1,8 @@
+﻿using Server.Domain.Entity;
+
+namespace Server.Application.Port;
+
+public interface IPlayerService
+{
+    Task<PlayerAccountData> CreatePlayerAsync(string playerId, string email, string password, string playerName);
+}
