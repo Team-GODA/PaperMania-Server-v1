5a86ed5998fbeb17118f1b7dc30813bc40b76063
Seanyee1227 <b01080080329@gmail.com>
2025-07-19 01:11:42 +0900

    delete: PlayerService 삭제
    #39

--- PaperMania/Server/Application/Port/IPlayerService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Server.Domain.Entity;
-
-namespace Server.Application.Port;
-
-public interface IPlayerService
-{
-    Task<PlayerAccountData> CreatePlayerAsync(string playerId, string email, string password, string playerName);
-}
