4d8abc33190279682fc188aafa3341a35f5037a5
Sean-mn <b01080080329@gmail.com>
2025-07-23 16:44:37 +0900

    refactor: 플레이어 캐릭터 관리 리포 생성
    #68

--- PaperMania/Server/Application/Port/IDataService.cs
+++ PaperMania/Server/Application/Port/IDataService.cs
@@ -6,12 +6,10 @@ public interface IDataService
 {
     Task<string> AddPlayerDataAsync(string playerName, string sessionId);
     Task<string?> GetPlayerNameByUserIdAsync(int userId, string sessionId);
-    Task<PlayerGameData?> GetByPlayerByIdAsync(int userId);
     Task<int> GetPlayerLevelByUserIdAsync(int userId, string sessionId);
     Task<int> GetPlayerExpByUserIdAsync(int userId, string sessionId);
     Task<PlayerGameData> UpdatePlayerLevelAsync(int userId, int level, int exp, string sessionId);
     Task<IEnumerable<PlayerCharacterData>> GetPlayerCharacterDataByUserIdAsync(int userId, string sessionId);
     Task<PlayerCharacterData> AddPlayerCharacterDataByUserIdAsync(PlayerCharacterData data, string sessionId);
     Task RenamePlayerNameAsync(int userId, string newPlayerName, string sessionId);
-    Task<PlayerGoodsData> GetPlayerGoodsDataByUserIdAsync(int userId, string sessionId);
 }
