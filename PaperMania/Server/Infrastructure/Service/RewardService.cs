61b67b0d8273c08432a0b2964db4dfe54c6fb224
Sean-mn <b01080080329@gmail.com>
2025-07-24 13:30:30 +0900

    feat: 플레이어 스테이지 보상 획득 서비스 로직 구현
    #67

--- PaperMania/Server/Infrastructure/Service/RewardService.cs
+++ PaperMania/Server/Infrastructure/Service/RewardService.cs
@@ -6,16 +6,33 @@ namespace Server.Infrastructure.Service;
 public class RewardService : IRewardService
 {
     private readonly IRewardRepository _rewardRepository;
+    private readonly IStageRepository _stageRepository;
 
-    public RewardService(IRewardRepository rewardRepository)
+    public RewardService(IRewardRepository rewardRepository, IStageRepository stageRepository)
     {
         _rewardRepository = rewardRepository;
+        _stageRepository = stageRepository;
     }
     
     public async Task<StageReward?> GetStageRewardAsync(int stageNum, int stageSubNum)
     {
         return await _rewardRepository.GetStageRewardAsync(stageNum, stageSubNum);
     }
-    
-    
+
+    public async Task ClaimStageRewardByUserIdAsync(int userId, StageReward reward, PlayerStageData data)
+    {
+        var stageReward = await _rewardRepository.GetStageRewardAsync(data.StageNum, data.SubStageNum);
+        if (stageReward == null)
+            throw new InvalidOperationException("해당 스테이지 보상을 찾을 수 없습니다.");
+        
+        if (await _stageRepository.IsClearedStageAsync(data))
+            stageReward.PaperPiece = 0;
+        else
+        {
+            data.IsCleared = true;
+            await _stageRepository.UpdateIsClearedAsync(data);
+        }
+        
+        await _rewardRepository.ClaimStageRewardByUserIdAsync(userId, stageReward);
+    }
 }
