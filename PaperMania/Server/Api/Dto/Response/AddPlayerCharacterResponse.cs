9e89d385857c97c86e41b352d0b0a70f073207ad
Seanyee1227 <b01080080329@gmail.com>
2025-07-26 19:34:05 +0900

    feat: 캐릭터 추가/조회 응답 Dto 생성
    #79

---
+++ PaperMania/Server/Api/Dto/Response/AddPlayerCharacterResponse.cs
@@ -0,0 +1,15 @@
+﻿using Server.Domain.Entity;
+
+namespace Server.Api.Dto.Response;
+
+public class AddPlayerCharacterResponse
+{
+    public int Id { get; set; }
+    public string CharacterId { get; set; }
+
+    public AddPlayerCharacterResponse(PlayerCharacterData data)
+    {
+        Id = data.Id;
+        CharacterId = data.CharacterId;
+    }
+}
---
+++ PaperMania/Server/Api/Dto/Response/GetAllPlayerCharactersResponse.cs
@@ -0,0 +1,13 @@
+﻿using Server.Domain.Entity;
+
+namespace Server.Api.Dto.Response;
+
+public class GetAllPlayerCharactersResponse
+{
+    public IEnumerable<PlayerCharacterData> PlayerCharacters { get; set; }
+
+    public GetAllPlayerCharactersResponse(IEnumerable<PlayerCharacterData> data)
+    {
+        PlayerCharacters = data;
+    }
+}
